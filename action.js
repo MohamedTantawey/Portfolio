8e8cde1e36d28ef89beadce1368902ca7f77fa6e
MohamedTantawey <146598892+MohamedTantawey@users.noreply.github.com>
2024-07-04 04:33:37 +0300

    Add Mohamed Gamal's Resume PDF and update action.js, index.html, and style.css
    - Added Mohamed Gamal's Resume PDF to Assets folder.
    - Updated action.js to improve functionality.
    - Modified index.html to enhance structure and content.
    - Adjusted style.css for improved design and responsiveness.

--- action.js
+++ action.js
@@ -4,14 +4,14 @@ const dynamicText = document.getElementById("constant-text");
 
 function typeText(text, callback) {
     let i = 0;
-    dynamicText.innerText = ''; // Clear the text
+    dynamicText.innerText = ''; 
     function type() {
         if (i < text.length) {
             dynamicText.innerText += text.charAt(i);
             i++;
-            setTimeout(type, 100); // Adjust typing speed here
+            setTimeout(type, 100); 
         } else if (callback) {
-            setTimeout(callback, 1000); // Wait before starting the next text
+            setTimeout(callback, 1000); 
         }
     }
     type();
@@ -23,9 +23,9 @@ function deleteText(callback) {
         if (text.length > 0) {
             text = text.substring(0, text.length - 1);
             dynamicText.innerText = text;
-            setTimeout(del, 100); // Adjust deleting speed here
+            setTimeout(del, 100); 
         } else if (callback) {
-            setTimeout(callback, 500); // Wait before typing the next text
+            setTimeout(callback, 500);
         }
     }
     del();
@@ -38,11 +38,10 @@ function startTypingEffect() {
                 currentIndex = (currentIndex + 1) % texts.length;
                 startTypingEffect();
             });
-        }, 1500); // Time to wait before starting to delete the text
+        }, 1500); 
     });
 }
 
-// Start the typing effect
 startTypingEffect();
 
 
